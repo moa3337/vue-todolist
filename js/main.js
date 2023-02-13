7161dc28368b343f560959e52c71ddff4e8c1d56
moa3337 <moa-x@hotmail.com>
2023-02-13 17:58:26 +0100

    final commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            newTask: "",
+            tasks: [
+                { text: "Imparare a disegnare", done: true },
+                { text: "Creare un art da zero", done: false },
+                { text: "publicare l'art", done: false }
+            ],
+            error: false,
+        }
+    },
+
+    methods: {
+        addTask() {
+            this.tasks.push({
+                text: this.newTask,
+                done: false
+            });
+            this.newTask = "";
+        },
+
+        removeTask(i) {
+            this.tasks.splice(i, 1);
+        },
+    },
+});
+
+app.mount("#app");
